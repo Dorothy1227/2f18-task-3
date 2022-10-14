7612ea28ab027d79c4144bcab1105ece99a3fd77
dorothy._1227 <115198953+Dorothy1227@users.noreply.github.com>
2022-10-14 09:32:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,76 @@
-basic.forever(function () {
-    basic.showNumber(1)
-    basic.pause(1000)
-    basic.showNumber(2)
-    basic.pause(1000)
-    basic.showNumber(3)
-    basic.pause(1000)
-    basic.showNumber(4)
-    basic.pause(1000)
-    basic.showNumber(5)
-    basic.pause(1000)
-    basic.showNumber(6)
-    basic.pause(1000)
+input.onButtonPressed(Button.A, function () {
+    counter = 0
 })
+input.onButtonPressed(Button.B, function () {
+    counter = 9
+    while (counter >= 0) {
+        basic.showNumber(counter)
+        counter += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # # # #
+        # # # # #
+        # . # . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        # # # # #
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
+let counter = 0
+basic.showString("2F18 Dorothy")
